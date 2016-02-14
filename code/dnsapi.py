2b614ad95fa234895fa0f5f675b2ed649e04e25d
Cristoffer Fairweather <developer@cfairweather.com>
2016-02-14 14:31:35 -0800

    The API has been created for use within other projects... still need a couple items.

---
+++ code/dnsapi.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import datetime
+import time
+from validators import IS_IN_SET
+from pydal import DAL, Field
+
+# Generate list of supported record types
+RECORD_TYPES = ['A', 'NS', 'CNAME', 'SOA', 'PTR', 'MX', 'TXT', 'RP', 'AFSDB', 'SIG', 'KEY', 'AAAA', 'LOC', 'SRV', 'NAPTR', 'KX', 'CERT', 'DNAME', 'OPT', 'APL', 'DS', 'SSHFP', 'IPSECKEY', 'RRSIG', 'NSEC', 'DNSKEY', 'DHCID', 'NSEC3', 'NSEC3PARAM', 'TLSA', 'HIP', 'SPF', 'TKEY', 'TSIG', 'IXFR', 'AXFR', 'ANY', 'TYPE257', 'TA', 'DLV']
+
+# I gave a lot of thought to this class name.
+# http://stackoverflow.com/questions/2853531/how-do-you-pep-8-name-a-class-whose-name-is-an-acronym
+class DNSAPI(object):
+	"""DNSAPI is an easy way to use your database so dockerlab-dns knows how to read your records. Use this class to interact with the database or just use it as an example."""
+	def __init__(self, db_user, db_pass, db_host, db_name, migrate=False):
+		super(DNSAPI, self).__init__()
+		self.db = DAL('postgres://%s:%s@%s/%s'%(db_user, db_pass, db_host, db_name), migrate=migrate)
+	
+		if self.db:
+		    print "Successfully connected to db \"%s\" on host \"%s\""%(db_name, db_host)
+
+		self.db.define_table('dns_zones',
+		    Field('name', 'string'), #ends in . (e.g. example.com.); input should probably have a validator to ensure zones end in a .
+		    )
+
+		self.db.define_table('dns_zone_records',
+		    Field('zone', 'reference dns_zones'),
+		    Field('record_name', 'string'), # (e.g. ns1.example.com.)
+		    Field('record_type', 'string', default = 'A', requires=IS_IN_SET(RECORD_TYPES)), # (e.g. A, AAAA, CNAME, MX, NS)
+		    Field('record_value', 'string'), # (e.g. an IP for A or AAAA, an address for CNAME, and an address and priority for MX)
+		    Field('record_ttl', 'integer', default = 60*5) #A TTL in seconds before a client should check for a new value. Can reasonably set to lower or higher depending on the volatility of the records
+		    )
+
+
+	def get_zones(self):
+		return self.db(self.db.dns_zones).select().as_list()
+
+	def get_zone(self, zone_name):
+		return self.db(self.db.dns_zones.name == zone_name).select().as_list()
+
+	def get_records(self, zone_id):
+		return self.db((self.db.dns_zone_records.zone == zone_id)).select().as_list()
+
+	def get_records_matching(self, zone_id, record_name=None, record_type=None):
+		q = (self.db.dns_zone_records.zone == zone_id)
+		if not record_name is None:
+			q = (q & (self.db.dns_zone_records.record_name == record_name))
+		if not record_type is None:
+			q = (q & (self.db.dns_zone_records.record_type == record_type))
+		return self.db(q).select().as_list()
+
+
+	def delete_zone(self, zone_name, and_records=True):
+		if and_records:
+			self.db((self.db.dns_zones.id == self.db.dns_zone_records.zone) & (self.db.dns_zones.name == zone_name)).delete()
+		return self.db(self.db.dns_zones.name == zone_name).delete()
+
+	def delecte_record(self, record_id):
+		return self.db((self.db.dns_zone_records.id == record_id)).delete()
+
+	def get_records_matching(self, zone_id, record_name=None, record_type=None):
+		q = (self.db.dns_zone_records.zone == zone_id)
+		if not record_name is None:
+			q = (q & (self.db.dns_zone_records.record_name == record_name))
+		if not record_type is None:
+			q = (q & (self.db.dns_zone_records.record_type==record_type))
+		return self.db(q).select().as_list()
+
+
+
--- code/dnsserver.py
+++ code/dnsserver.py
@@ -14,14 +14,15 @@ import traceback
 import SocketServer
 import struct
 import os
+from validators import IS_IN_SET
 from pydal import DAL, Field
+from dnsapi import DNSAPI
 try:
     from dnslib import *
 except ImportError:
     print("Missing dependency dnslib: <https://pypi.python.org/pypi/dnslib>. Please install it with `pip`.")
+    print("If running inside dockerlab-dns, this dependency should already be installed if using the correct Dockerfile.")
     sys.exit(2)
-
-
 
 # using get will return `None` if a key is not present rather than raise a `KeyError`
 db_host = os.environ.get('POSTGRES_HOST')
@@ -29,38 +30,19 @@ db_name = os.environ.get('POSTGRES_DB')
 db_user = os.environ.get('POSTGRES_USER')
 db_pass = os.environ.get('POSTGRES_PASSWORD')
 
-if not (db_name and db_user and db_pass):
+if not (db_name and db_user and db_pass and db_host):
     print "You kinda need db creds first, k?"
-    print "Please include as environment (env_file or environment in docker-compose) POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, and POSTGRES_PASSWORD so dockerlab-dns can properly server DNS records."
+    print "Please include as environment (env_file or environment in docker-compose) POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, and POSTGRES_PASSWORD so dockerlab-dns can properly serve DNS records."
     sys.exit(2)
 
 
-#Connect with migrate = False  and define tables so DAL can use the schema natively
-db = DAL('postgres://%s:%s@%s/%s'%(db_user, db_pass, db_host, db_name), migrate=True)
-if db:
-    print "Successfully connected to db \"%s\" on host \"%s\""%(db_name, db_host)
-
-db.define_table('dns_zones',
-    Field('name', 'string'), #ends in . (e.g. example.com.); input should probably have a validator to ensure zones end in a .
-    )
-
-db.define_table('dns_zone_records',
-    Field('zone', 'reference dns_zones'),
-    Field('record_name', 'string'), # (e.g. ns1.example.com.)
-    Field('record_type', 'string', default = 'A'), # (e.g. A, AAAA, CNAME, MX, NS)
-    Field('record_value', 'string'), # (e.g. an IP for A or AAAA, an address for CNAME, and an address and priority for MX)
-    Field('record_ttl', 'integer', default = 60*5) #A TTL in seconds before a client should check for a new value. Can reasonably set to lower or higher depending on the volatility of the records
-    )
+dnsapi=DNSAPI(db_user, db_pass, db_host, db_name, False)
 
 
 class DomainName(str):
     def __getattr__(self, item):
         return DomainName(item + '.' + self)
 
-
-# D = DomainName('example.com.')
-# IP = '127.0.0.1'
-# TTL = 60 * 5
 
 # soa_record = SOA(
 #     mname=D.ns1,  # primary name server
@@ -100,7 +82,7 @@ def dns_response(data):
     # if qn == D or qn.endswith('.' + D):
     # Sheiße, multi `.` tlds suck (e.g. .co.uk)
     zone=None
-    for z in db(db.dns_zones).select():
+    for z in dnsapi.get_zones():
         if qn == z.name or qn.endswith('.' + z.name):
             zone=z
             break
@@ -109,7 +91,7 @@ def dns_response(data):
         print "Found zone: %s"%(zone)
 
         # Lookup records in zone. Required for wildcard queries
-        for record in db((db.dns_zone_records.zone == zone.id) & (db.dns_zone_records.record_name == qn)).select():
+        for record in dnsapi.get_records_matching(zone.id, record_name=qn):
             rqt = record.record_type
             print "Possible record in zone (%s): %s: %s"%(qn, record.record_type, record.record_value)
 
@@ -118,20 +100,23 @@ def dns_response(data):
                 print "Found record in zone (%s): %s: %s "%(qn, record.record_type, record.record_value)
 
                 # Be sure requested type is indeed in dnslib
-                if qt in dir(dns):
+                if qt in dir(dns) and qt in RECORD_TYPES:
                     record_value = record.record_value.split(" ")
                     # Handles MX priorities explicitly or implicitly and other records
-                    reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=record.record_ttl, rdata=getattr(dns,qt)(record_value[1], int(record_value[0])) if record.record_type == 'MX' and len(record_value) > 1 else getattr(dns,qt)(record_value[0])))
+                    reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=record.record_ttl, rdata=getattr(dns,rqt)(record_value[1], int(record_value[0])) if record.record_type == 'MX' and len(record_value) > 1 else getattr(dns,qt)(record_value[0])))
                 else:
                     print "Error: Requested a non DNS record type: %s"%(qt)
 
-        for rdata in db((db.dns_zone_records.id == zone.id) & (db.dns_zone_records.record_type == 'NS')).select():
+        for rdata in dnsapi.get_records_matching(zone.id, record_type='NS'):
             reply.add_ar(RR(rname=zone.name, rtype=QTYPE.NS, rclass=1, ttl=record.record_ttl, rdata=NS(rdata.record_value)))
 
-        SOA_rec=db((db.dns_zone_records.id == zone.id) & (db.dns_zone_records.record_type == 'SOA')).select().first()
+        SOA_rec=dnsapi.get_records_matching(zone.id, record_type='SOA')
         if SOA_rec:
             SOA_vals = SOA_rec.record_value.split(" ")
-            reply.add_auth(RR(rname=zone.name, rtype=QTYPE.SOA, rclass=1, ttl=SOA_rec.record_ttl, rdata=SOA(SOA_vals[0], SOA_vals[1], times=tuple([int(n) for n in SOA_vals[2:]]))))
+
+            mname, rname = SOA_vals[:2]
+            times = tuple([int(n) for n in SOA_vals[2:]])
+            reply.add_auth(RR(rname=zone.name, rtype=QTYPE.SOA, rclass=1, ttl=SOA_rec.record_ttl, rdata=SOA(mname, rname, times=times)))
 
     print("---- Reply:\n", reply)
 
